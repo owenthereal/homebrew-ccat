17ef3e73fcff59a2857b8574d9a73dc33a7d6fbe
Jingwen Owen Ou <jingweno@gmail.com>
2015-05-06 08:32:23 -0700

    Update build script from main repo

--- ccat.rb
+++ ccat.rb
@@ -3,7 +3,7 @@ require "formula"
 class Ccat < Formula
   homepage "https://github.com/jingweno/ccat"
   url "https://github.com/jingweno/ccat/archive/v0.0.2.tar.gz"
-  sha1 "a0c966ab9a2edfad96c985c2792aba20f842cf17"
+  sha1 "1bf46bb96d483454462b373966ef48867961eecb"
 
   depends_on "go" => :build
 
