4749f64541d64d003ea549325902250cba4f8eeb
Jingwen Owen Ou <jingweno@gmail.com>
2015-05-06 07:24:15 -0700

    Fix `go build` issue

--- ccat.rb
+++ ccat.rb
@@ -2,13 +2,13 @@ require "formula"
 
 class Ccat < Formula
   homepage "https://github.com/jingweno/ccat"
-  url "https://github.com/jingweno/ccat/archive/v0.0.1.tar.gz"
-  sha1 "87649870c2191494df98cdcd9c37db0bb11f7313"
+  url "https://github.com/jingweno/ccat/archive/v0.0.2.tar.gz"
+  sha1 "a0c966ab9a2edfad96c985c2792aba20f842cf17"
 
   depends_on "go" => :build
 
   def install
-    system "go build -o ccat"
+    system "./build"
     bin.install "ccat"
   end
 end
