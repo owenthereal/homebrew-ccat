4accb0bad48f5b7c062df567ac2cf859fa05aa21
Jingwen Owen Ou <jingweno@gmail.com>
2015-05-11 11:29:59 -0700

    Bump to 0.3.0

--- ccat.rb
+++ ccat.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Ccat < Formula
   homepage "https://github.com/jingweno/ccat"
-  url "https://github.com/jingweno/ccat/archive/v0.0.3.tar.gz"
-  sha1 "1c903640ccb64e15064e5e3fdf08871d0289bfb0"
+  url "https://github.com/jingweno/ccat/archive/v0.1.0.tar.gz"
+  sha1 "3d29f2204ed1b435408294b4ff96da2e6a47ce74"
 
   depends_on "go" => :build
 
